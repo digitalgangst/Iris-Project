6fb6b3226933e9610559b6edc4e91608c5437dfc
Viktor [t1m3] <t1m3root@protonmail.com>
2019-10-14 06:34:37 -0300

    Minor changes for Magic Mirror

--- iris.py
+++ iris.py
@@ -4,18 +4,37 @@
 ###IMPORT
 
 import snowboydecoder, pyttsx3, os.path, wave, requests, os
-import pyowm, time
-import threading, re, wikipedia, json, pyowm
+from datetime import datetime, date
 import speech_recognition as iris
 from subprocess import call
 from google_speech import Speech
 from bs4 import BeautifulSoup
+import threading, re, wikipedia, json, pyowm
 from newsapi.newsapi_client import NewsApiClient
-from pynput.keyboard import Key, Controller
 from urllib.request import urlopen
-from time import gmtime, strftime, time
 
-###Configs
+
+### Blank Screen / active powersave mode
+class blackscreen(threading.Thread):
+    def run(self):
+        os.system("eog --fullscreen iris_blank/black.jpg")
+        pass
+thread0 = blackscreen()
+thread0.daemon = True
+thread0.start()
+
+logo = ('''
+
+
+██╗    ██████╗     ██╗    ███████╗
+██║    ██╔══██╗    ██║    ██╔════╝
+██║    ██████╔╝    ██║    ███████╗
+██║    ██╔══██╗    ██║    ╚════██║
+██║    ██║  ██║    ██║    ███████║
+╚═╝    ╚═╝  ╚═╝    ╚═╝    ╚══════╝
+
+
+''')
 
 isfile = os.path.isfile('master_name')
 if isfile == True:
@@ -28,12 +47,14 @@ else:
 
 # speech config
 speech_engine = pyttsx3.init()
-speech_engine.setProperty('rate', 50) # noise config
+speech_engine.setProperty('rate', 150) # noise config
 print('[*] Ajustando ruído de fundo.')
 recognizer = iris.Recognizer()
 with iris.Microphone() as source:
     recognizer.adjust_for_ambient_noise(source)
-print('[+] OK./n')
+call(['clear'])
+print(logo)
+print('[+] Ruídos ajustados.')
 # key pressing config
 keyboard = Controller()
 #API Keys
@@ -41,19 +62,23 @@ newsapi = NewsApiClient(api_key='f88b2cec4f5d4e46a511706325e756de')
 climaAPI = ('c5743b7436f81e619263925dca22e064')
 
 def func():
+
     master_name = open('master_name', 'r').read()
     lang = 'pt-br'
 
+    ### know commands
     music_w = ['Toque', 'toque', 'Tocar', 'tocar', 'música', 'som']
-    music_y = ['Recomenda', 'recomenda', 'Recomende', 'recomende', 'Recomendação', 'recomendação']
     wiki_w = ['Quem é', 'quem é', 'Quem foi', 'quem foi', 'O que é', 'o que é']
     news_w = ['Noticias', 'noticias', 'Notícias', 'notícias', 'Notícia', 'notícia']
     wheater_w = ['Clima', 'Previsão', 'Tempo', 'Temperatura', 'clima', 'previsão', 'tempo', 'temperatura']
+    funcs = ['Fazer', 'Funções', 'Comandos', 'fazer', 'funções', 'comandos' ]
+    disp_w = ['Manter', 'Mantenha', 'Ligada', 'manter', 'mantenha', 'ligada']
+    disp_o_w = ['Desligar', 'Escurecer', 'desligar', 'escurecer']
     price_quote_w = ['Cotação', 'cotação', 'Valor', 'valor', 'Preço', 'preço']
 
     if any(x in l for x in price_quote_w):
         def search_currency(currency):
-            with open('./json/moedas.json') as crrncy:
+            with open('~/json/moedas.json') as crrncy:
                 data = json.load(crrncy)
                 data = data[currency]
                 def api_price(data):
@@ -62,14 +87,22 @@ def func():
                     price_quote = 'A cotação atual é de R$ '+price[data]['bid'][:4]
                     Speech(price_quote, lang).play()
                 api_price(data)
-        try:
-            currency = re.findall(r'\w+$', l)[0]
-            search_currency(currency)
-        except:
-            error = ("Erro na API, tente novamente mais tarde.")
-            print(error)
-            Speech(error, lang).play()
 
+    if any(x in l for x in disp_o_w):
+        class blackscreen1(threading.Thread):
+            def run(self):
+                os.system("eog --fullscreen iris_blank/black.jpg")
+                pass
+        thread1 = blackscreen1()
+        thread1.daemon = True
+        thread1.start()
+        Speech('Entendido.', lang).play()
+    if any(x in l for x in disp_w):
+        call(["killall", "eog"])
+        Speech('Entendido.', lang).play()
+
+    if any (x in l for x in funcs):
+        Speech('Posso te informar a previsão do tempo, as horas, as ultimas notícias, tocar e recomendar músicas de acordo como gênero desejado e também fazer pesquisas usando o wikipédia.',lang).play()
     if any(x in l for x in wheater_w):
         url = 'http://ipinfo.io/json'
         response = urlopen(url)
@@ -78,7 +111,7 @@ def func():
 
         print(city1)
 
-        
+        import pyowm, time
         owm = pyowm.OWM(climaAPI)
         city = owm.weather_at_place('{0}, BR'.format(city1))
         weather = city.get_weather()
@@ -94,6 +127,7 @@ def func():
         response = ('A temperatura em %s é de %d°' % (city1, temp))
 
         Speech(response, lang).play()
+        time.sleep(0.5)
         Speech(response_x, lang).play()
 
     if any(x in l for x in wiki_w):
@@ -109,49 +143,54 @@ def func():
             Speech(text, lang).play()
 
     if ('horas') in l:
-        current_time = strftime('%H:%M', gmtime())
-        Speech('Agora são {0}'.format(current_time), lang).play()
+        now = datetime.now()
+        h = now.hour
+        m = now.minute
+        d = now.day
+        mm = now.month
+        y = now.year
+
+        DIAS = [
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-Feira',
+        'Sexta-feira',
+        'Sábado',
+        'Domingo'
+        ]
+
+        MESES = {1:'Janeiro', 2:'Fevereiro', 3:'Março', 4:'Abril',
+                5:'Maio', 6:'Junho', 7:'Julho', 8:'Agosto',
+                9:'Setembro', 10:'Outubro', 11:'Novembro', 12:'Dezembro'}
+
+        data = date(year=y, month=m, day=d)
+        indice_da_semana = data.weekday()
+        dia_da_semana = DIAS[indice_da_semana]
+
+        mes1 = MESES.get(m)
+        dia = ('%s, %d de %s' % (dia_da_semana, d, mes1))
+        
+        Speech('Agora são %d:%d. Hoje é %s.' % (h, m, dia), lang).play()
 
     if any(x in l for x in music_w):
         if ('Pausar') in l or ('pausar') in l:
-            keyboard.press(Key.space)
-            keyboard.release(Key.space)
+            call(["spotifycli", "--pause"])
             Speech('Música pausada.', lang).play()
         elif ('Retomar') in l or ('retomar') in l or ('Continuar') in l or ('continuar' in l):
             Speech('Retomando música.', lang).play()
-            keyboard.press(Key.space)
-            keyboard.release(Key.space)
+            call(["spotifycli", "--play"])
         elif ('Parar') in l or ('parar') in l:
-            call(["killall", 'tizonia'])
+            call(["killall", 'spotify'])
             Speech('Música interrompida.', lang).play()
-        elif any(y in l for y in music_y):
-            Speech('Qual gênero de música você deseja?', lang).play()
-            with iris.Microphone() as source:
-                audio = recognizer.listen(source)
-                try:
-                    genre = recognizer.recognize_google(audio)
-                    print(genre)
-
-                    class Spotify(threading.Thread):
-                        def run(self):
-                            os.system("tizonia --spotify-recommendations-by-genre '%s'" % (genre))
-                            pass
-                    thread = Spotify()
-                    thread.daemon = True
-                    Speech('Ok, tocando músicas recomendadas do gênero {0}.'.format(genre), lang).play()
-                    thread.start()
-
-                except Exception as e:
-                    print(e)
-                    Speech('Desculpe, não consegui encontrar.', lang).play()
 
         else:
             try:
-                s = os.system("pgrep tizonia")
+                s = os.system("pgrep spotify")
                 if s == 0:
                     print('Processo ativo.')
-                    os.system('killall tizonia')
-                    Speech('Já estou com um processo de reprodução de música ativo. Irei terminar-lo.', lang).play()
+                    os.system('killall spotify >/dev/null 2>/dev/null')
+                    Speech('Música interrompida.', lang).play()
                 else:
                     Speech('Qual música gostaria de ouvir?', lang).play()
                     with iris.Microphone() as source:
@@ -175,7 +214,8 @@ def func():
 
                             class Spotify(threading.Thread):
                                 def run(self):
-                                    os.system("tizonia --spotify-track-id %s" % (uri))
+                                    os.system("spotify --uri='spotify:track:%s#0:01' & xdotool search --sync --onlyvisible --class 'spotify' windowminimize" % (uri))
+                                    os.system("spotifycli --pause && spotifycli --play") # show in mirror
                                     pass
                             thread = Spotify()
                             thread.daemon = True
@@ -203,6 +243,7 @@ def listen():
 
 def detected_callback():
     global l
+    os.system("killall eog >/dev/null 2>/dev/null") # screen on
     print('[+] Listening...')
     call(["play", "beep-07.wav", "-q"])
     l = listen()
@@ -210,9 +251,8 @@ def detected_callback():
 
     if l != '':
         func()
-
-
-
+        call(['clear'])
+        print(logo)
 
 detector = snowboydecoder.HotwordDetector("Iris.pmdl", sensitivity=0.5, audio_gain=1)
 detector.start(detected_callback)
